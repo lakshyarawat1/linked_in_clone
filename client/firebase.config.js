4ee9fe4b1fa5f12bfd80ce22e8238d834d20c108
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-11-27 22:05:07 +0530

    Confiurated Vite and TailwindCSS

---
+++ client/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqIutAbQ-u7zRJLegMA3ipkECx9X1FsOE",
+  authDomain: "linked-in-clone-36ea5.firebaseapp.com",
+  projectId: "linked-in-clone-36ea5",
+  storageBucket: "linked-in-clone-36ea5.appspot.com",
+  messagingSenderId: "146077554654",
+  appId: "1:146077554654:web:1a60dd1d1784a7694f2f4f",
+  measurementId: "G-9PF7ZVFX3S",
+};
+
+export const app = initializeApp(firebaseConfig);
